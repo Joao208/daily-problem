62c44420285fa48505ba205ffec1390ba3b2aae9
João Augusto <joao.augusto@foodzilla.com.br>
2022-02-27 17:34:44 -0300

    feat: adding day twenty and eslint

--- src/scripts/#20/index.js
+++ src/scripts/#20/index.js
@@ -6,42 +6,42 @@
  * @time 0.2ms
  */
 function dayTwenty() {
-  console.log('Expected: 2')
-  console.time('Tempo de execução')
+  console.log('Expected: 2');
+  console.time('Tempo de execução');
 
   class Solution {
     minSubArrayLen(nums, s) {
-      let l = 0
-      let r = -1
-      let sum = 0
-      let res = nums.length + 1
+      let l = 0;
+      let r = -1;
+      let sum = 0;
+      let res = nums.length + 1;
 
       while (l < nums.length) {
         if (sum < s && r + 1 < nums.length) {
-          r++
-          sum += nums[r]
+          r++;
+          sum += nums[r];
         } else {
-          sum -= nums[l]
-          l++
+          sum -= nums[l];
+          l++;
         }
 
         if (sum >= s) {
-          res = Math.min(res, r - l + 1)
+          res = Math.min(res, r - l + 1);
         }
       }
 
       if (res == nums.length + 1) {
-        return 0
+        return 0;
       }
 
-      return res
+      return res;
     }
   }
 
-  const result = new Solution().minSubArrayLen([2, 3, 1, 2, 4, 3], 7)
+  const result = new Solution().minSubArrayLen([2, 3, 1, 2, 4, 3], 7);
 
-  console.log(`Result: ${result}`)
-  console.timeEnd('Tempo de execução')
+  console.log(`Result: ${result}`);
+  console.timeEnd('Tempo de execução');
 }
 
-module.exports = dayTwenty
+module.exports = dayTwenty;
